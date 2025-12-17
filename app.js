33fa87b41b71bcf7f012204835206dddc3256463
MedSlahB <med.slah.23@gmail.com>
2025-12-17 10:41:07 +0100

    feat: upgrade app to version 2.0.0

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Test maybe 123");
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <html>
+      <body style="background-color: #2d3436; color: #dfe6e9; font-family: sans-serif; text-align: center; padding-top: 50px;">
+        <h1 style="color: #00cec9;">ArgoCD + Tekton + Ansible</h1>
+        <h2 style="border-bottom: 2px solid #00cec9; display: inline-block; padding-bottom: 10px;">
+          Application Version 2.0.0
+        </h2>
+        <p>Status: <span style="color: #55efc4;">Successfully Updated via Pipeline</span></p>
+        <p>Build Time: ${new Date().toLocaleString()}</p>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(8080, () => {
-  console.log('App running on port 8080');
+  console.log('App v2.0.0 running on port 8080');
 });
 
